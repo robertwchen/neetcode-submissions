6bd0502d70a29309f8b16983d768175b2b4c1380
robertwchen <137726658+robertwchen@users.noreply.github.com>
2026-07-21 02:16:47 -0400

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = self.build_graph(numCourses, prerequisites)
+        num_parents = self.build_parents(numCourses, prerequisites)
+
+        queue = deque([])
+        for course in num_parents:
+            if num_parents[course] == 0:
+                queue.append(course)
+
+        result = []
+        while queue:
+            course = queue.popleft()
+            
+            for child in graph[course]:
+                num_parents[child] -= 1
+                if num_parents[child] == 0:
+                    queue.append(child)
+            result.append(course)
+
+        if len(result) != len(num_parents):
+            return []
+
+        return result
+
+    def build_graph(self, n, edges):
+        graph = {}
+        for i in range(n):
+            graph[i] = []
+        for edge in edges:
+            a, b = edge
+            graph[b].append(a)
+        return graph
+    def build_parents(self, n, edges):
+        graph = {}
+        for i in range(n):
+            graph[i] = 0
+        for edge in edges:
+            a, b = edge
+            graph[a] += 1
+        return graph
+        
+        
