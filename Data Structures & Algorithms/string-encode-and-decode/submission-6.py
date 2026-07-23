315b1fbfd1af4306e53c455f9d8cab1bd0fbcd1f
robertwchen <137726658+robertwchen@users.noreply.github.com>
2026-07-23 01:20:02 -0400

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,40 @@
+# where am I
+    # at some point on the string
+# what am I doing
+# what do I return
+
+
+class Solution:
+# turn ["a", "bb"] into 1#a2#bb
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for s in strs:
+            s_len = str(len(s))
+            encoded.append(s_len + '#' + s)
+        encoded_s = "".join(encoded)
+        return encoded_s
+
+    5#Hello5#World
+    def decode(self, s: str) -> List[str]:
+        # loop through chars 
+        decoded = []
+        ptr = 0
+        print(len(s))
+        while ptr < len(s):
+            print("hello")
+            num_s = ""
+            while s[ptr] != '#':
+                num_s += s[ptr]
+                ptr += 1
+
+            ptr += 1 #skip 
+            num = int(num_s)
+            word = ""
+            for i in range(num):
+                word += s[ptr]
+                ptr += 1
+            decoded.append(word)
+        return decoded
+
+
+
