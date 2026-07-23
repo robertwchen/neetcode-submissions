35ae18e193fa520d5c742490fdc95848c363e839
robertwchen <137726658+robertwchen@users.noreply.github.com>
2026-07-23 01:27:37 -0400

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,26 @@
+# where am I
+    # at some point on array
+# what am I doing
+    # checking if the previous number exists
+# what do I return
+    # the longest substring
+
+# brute force: look at every point on array and check if the next one exists incrementing
+# or sort the array and lookfor any breaks and take the p1 -> p2 length
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+
+        max_len = 0
+        for num in nums:
+            curr_len = 0
+            if (num - 1) not in nums_set:
+                while num in nums_set:
+                    num += 1
+                    curr_len += 1
+                max_len = max(curr_len, max_len) 
+        return max_len
+            
+
+        
